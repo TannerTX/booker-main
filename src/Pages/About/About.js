f1310cd542449ad2b3ad2ce31f6b45c23fb67b2b
TannerTX <tanner_bibb@yahoo.com>
2024-05-30 14:07:30 -0500

    Added media preview

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import "./About.css";
-import BONNER from '../../Media/videos/BONNER.webm';
 
 export default function About() {
-    const [imageData, setImageData] = useState([]);
+    const [mediaData, setMediaData] = useState([]);
     const [messages, setMessages] = useState([]);
 
     const handleDirectoryUpload = async (event) => {
         const files = event.target.files;
         const fileArray = Array.from(files);
         const jsonFiles = fileArray.filter(file => file.name === 'messages.json');
-        const imageContents = [];
+        const mediaContents = [];
         const messageContents = [];
 
         for (const file of jsonFiles) {
@@ -19,39 +18,64 @@ export default function About() {
 
             jsonData.forEach(item => {
                 if (item.Attachments && item.Attachments.includes('cdn.discordapp.com')) {
-                    imageContents.push({ link: item.Attachments, timestamp: item.Timestamp });
+                    mediaContents.push({ link: item.Attachments, timestamp: item.Timestamp });
                 } else if (item.Contents) {
                     messageContents.push(item.Contents);
                 }
             });
         }
 
-        // Sort imageContents by timestamp in descending order
-        imageContents.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+        // Sort mediaContents by timestamp in descending order
+        mediaContents.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
 
-        setImageData(imageContents);
+        setMediaData(mediaContents);
         setMessages(messageContents);
     };
 
-    const isImageLink = (link) => {
+    const isMediaLink = (link) => {
         const imageExtensions = ['jpeg', 'jpg', 'gif', 'png'];
-        return imageExtensions.some(ext => link.toLowerCase().includes(`.${ext}`));
+        const videoExtensions = ['mov', 'mp4'];
+        return imageExtensions.some(ext => link.toLowerCase().includes(`.${ext}`)) || 
+               videoExtensions.some(ext => link.toLowerCase().includes(`.${ext}`));
+    };
+
+    const isVideoLink = (link) => {
+        const videoExtensions = ['mov', 'mp4', 'webm'];
+        return videoExtensions.some(ext => link.toLowerCase().includes(`.${ext}`));
+    };
+
+    const triggerFileUpload = () => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.webkitdirectory = true;
+        input.directory = true;
+        input.multiple = true;
+        input.onchange = handleDirectoryUpload;
+        input.click();
     };
 
     return (
-        <>
-            <input
-                className="inputBtn" 
-                type="file" 
-                webkitdirectory="true" 
-                directory="true" 
-                multiple 
-                onChange={handleDirectoryUpload}
-            />
-            <div className="image-preview">
-                {imageData.map((item, index) => (
-                    <div key={index} className="image-container">
-                        {isImageLink(item.link) ? (
+        <div className="parent-container">
+            <button 
+                className="bn632-hover bn21"
+                onClick={triggerFileUpload}
+            >
+                Upload Directory
+            </button>
+            <div className="media-preview">
+                {mediaData.map((item, index) => (
+                    <div key={index} className="media-container">
+                        {isVideoLink(item.link) ? (
+                            <>
+                            <p>{item.timestamp}</p>
+                            <video controls className="preview-media">
+                                <source src={item.link} type="video/mp4" />
+                                <source src={item.link} type="video/quicktime" />
+                                Your browser does not support the video tag.
+                            </video>
+                            <br />
+                            </>
+                        ) : isMediaLink(item.link) ? (
                             <>
                             <p>{item.timestamp}</p>
                             <img src={item.link} alt={item.timestamp} className="preview-image" />
@@ -65,6 +89,6 @@ export default function About() {
                     </div>
                 ))}
             </div>
-        </>
+        </div>
     );
 }
