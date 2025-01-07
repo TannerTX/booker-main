21a6779e80fee2cabfcdef10186d3f1d88310f5b
TannerTX <tanner_bibb@yahoo.com>
2025-01-06 19:07:23 -0600

    Fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React from "react"
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Homepage from "./Pages/Homepage/Homepage.js"
-import ReservePage from "./Pages/ReservePage/ReservePage.js"
-import Gabe from "./Pages/Gabe/Gabe.js"
-import History from "./Pages/History/History.js"
-import Jobs from "./Pages/Jobs/Jobs.js"
-import Navbar from './Components/Navbar/Navbar.js';
-import Insert from './Pages/Insert/Insert.js'
-import About from './Pages/About/About.js'
-import Policy from './Pages/Policy/Policy.js'
-import Dictionary from './Pages/Dictionary/Dictionary.js';
+// import { BrowserRouter, Routes, Route} from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+
+// import Homepage from "./Pages/Homepage/Homepage.js"
+// import ReservePage from "./Pages/ReservePage/ReservePage.js"
+// import Gabe from "./Pages/Gabe/Gabe.js"
+// import History from "./Pages/History/History.js"
+// import Jobs from "./Pages/Jobs/Jobs.js"
+// import Navbar from './Components/Navbar/Navbar.js';
+// import Insert from './Pages/Insert/Insert.js'
+// import About from './Pages/About/About.js'
+// import Policy from './Pages/Policy/Policy.js'
+// import Dictionary from './Pages/Dictionary/Dictionary.js';
 
 function App() {
 
 
   return (
     <>
-    <BrowserRouter>
-      <Navbar />
+    {/* <BrowserRouter> */}
+      {/* <Navbar /> */}
       {/* <Routes> 
         <Route index element={<Homepage />} />
         <Route path="/Reserve" element={<ReservePage />} />
@@ -31,7 +33,7 @@ function App() {
         <Route path="/Policy" element={<Policy />} />
         <Route path="/Dictionary" element={<Dictionary />} />
       </Routes> */}
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
     </>
 
   )
